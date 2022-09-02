4e979679c584ad52a8acb77a1ecf969ed616f2d2
Shresth Jaiswal <shresthjaiswal193@gmail.com>
2022-09-02 19:50:34 +0530

    first comm

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
-
+const cors = require("cors")
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes')
 const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
@@ -37,6 +37,6 @@ if ("production" === "production") {
 app.use(notFound);
 app.use(errorHandler);
 app.use(errorHandler);
-
+app.use(cors)
 const Port = process.env.PORT||5000
 app.listen(Port,console.log(`Server started on port ${Port}`));
